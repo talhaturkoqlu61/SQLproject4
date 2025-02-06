295efa4031711796b2317710a6c58a3d2386eabb
Talha Türkoğlu <158575414+talhaturkoqlu61@users.noreply.github.com>
2025-02-06 18:40:47 +0300

    PatikaDev

---
+++ SQLproject4.sql
@@ -0,0 +1,12 @@
+--SELECT DISTINCT replacement_cost FROM film;(1.question)
+
+--SELECT COUNT(DISTINCT replacement_cost)FROM film;(2.question)
+
+--SELECT COUNT (*)FROM film
+--WHERE title LIKE 'T%'AND rating='G';(3.question)
+
+--SELECT COUNT (*) FROM country
+--WHERE country LIKE '_____';(4.question)
+
+--SELECT COUNT(*) FROM city
+--WHERE city ILIKE '%R';(5.question)
